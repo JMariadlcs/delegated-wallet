1cb71a1eb36aa10faadf6a9a818df7cc82240bf9
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-12-07 11:36:09 +0100

    format contract

--- src/DelegationIndexer.sol
+++ src/DelegationIndexer.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.20;
+
 contract DelegationIndexer {
 
     /// @notice Struct for ERC 721 token delegations
@@ -17,7 +18,7 @@ contract DelegationIndexer {
     event ERC721Delegated(address indexed from, address indexed to, address indexed contractAddress, uint256 tokenId);
 
     function ERC721Delegation(address to, address contractAddress, uint256 tokenId, bool activeDelegation) external returns (bytes32 ERC721DelegationHash) {
-        ERC721DelegationHash =  keccak256(abi.encode(msg.sender, to, contractAddress, tokenId));
+        ERC721DelegationHash = keccak256(abi.encode(msg.sender, to, contractAddress, tokenId));
 
         if (activeDelegation) ERC721Delegations[msg.sender][ERC721DelegationHash] = true;
         else ERC721Delegations[msg.sender][ERC721DelegationHash] = false;
