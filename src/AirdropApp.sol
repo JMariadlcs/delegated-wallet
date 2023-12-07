6f2e6348f039e97947bc618a32143041fa739011
Jose María de la Cruz <jmariadelacruzsanchez@gmail.com>
2023-12-07 11:58:23 +0100

    add function comments

--- src/AirdropApp.sol
+++ src/AirdropApp.sol
@@ -6,18 +6,23 @@ import "../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 import "../lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 import "./interfaces/IDelegationIndexer.sol";
 
-import "forge-std/console.sol";
-
 error NotElegible();
 
 contract AirdropApp is ERC20 {
+
     address delegationIndexerAddress;
     address ERC721ContractAddress;
     uint256 tokenId;
     uint256 airdropTokenAmount;
 
     event AirdropClaimed(address receiver);
-
+    
+    /** 
+     * @param _delegationIndexerAddress DelegationIndexer contract for checking in any ERC721 has been delegated
+     * @param _ERC721ContractAddress address of the ERC721 token elegible for airdrop
+     * @param _tokenId Id of the ERC721 token elegible for airdrop
+     * @param _aidropTokenAmount amount of ADRP tokens that elegible user is going to receive when claiming
+     */
     constructor(
         address _delegationIndexerAddress,
         address _ERC721ContractAddress,
@@ -30,6 +35,7 @@ contract AirdropApp is ERC20 {
         airdropTokenAmount = _aidropTokenAmount;
     }
 
+    /// @notice function for claiming airdrop. Owners of ERC721 and tokenId or delegated addresses by owners are elegible
     function claimAirdrop() external {
         address ERC721Owner = ERC721(ERC721ContractAddress).ownerOf(tokenId);
         
